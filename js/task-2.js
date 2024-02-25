95c7641c20abfff685b8a457973f21cb3721bc9a
Bohdana28 <bog.olhowetzcka@gmail.com>
2024-02-25 22:36:51 +0000

    hw 4

---
+++ js/task-2.js
@@ -0,0 +1,67 @@
+'use strict'
+function calcAverageCalories(days) {
+    const cal = [];
+    for (const calorie of days) {
+        cal.push(calorie.calories);
+    }
+    let totalCal = 0
+    for (const key of cal) {
+        totalCal += key
+    }
+    if (totalCal > 0) {
+        return totalCal / cal.length
+    } else {
+        return totalCal
+    }
+
+}
+
+calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+]);
+  
+calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+]);
+calcAverageCalories([]);
+
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
